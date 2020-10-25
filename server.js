84a596fcadd5e8b1d33da2659d73507b4c2eded6
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-10-24 20:17:53 -0400

    Add mongo to server and add read action

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const budgetItemsModel = require("./models/budget_items_schema");
+const budget = require("./budget.json");
+
 const app = express();
 const port = 3000;
+const mongoUrl = "mongodb://localhost:27017/budget";
 
 app.use(cors());
-
-const budget = require("./budget.json");
 
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/budget", (req, res) => {
-  res.json(budget);
+  // Old code to read from local JSON file
+  // res.json(budget);
+
+  // Connect to the mongo database
+  mongoose
+    .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      // Find all data on the budget_items collection
+      budgetItemsModel
+        .find({})
+        .then((data) => {
+          res.json(data);
+          mongoose.connection.close();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(port, () => {
